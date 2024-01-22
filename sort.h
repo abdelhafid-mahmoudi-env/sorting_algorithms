7ac58a47ee3a8495f5266d9aafb3a68d012d6fb7
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-01-22 10:55:27 -0800

    test

---
+++ sort.h
@@ -0,0 +1,18 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+
+/* Data structure for doubly linked list */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
