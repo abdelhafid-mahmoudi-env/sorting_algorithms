41586bf21e738b246babca6fe2016a4b601ecb55
Eltrone <amineelorche@gmail.com>
2024-01-23 16:38:13 -0800

    file added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,37 +2,37 @@
 
 /**
 * insertion_sort_list - Sorts a doubly linked list of integers
-*                       in ascending order using the Insertion sort algorithm
+* in ascending order using the Insertion sort algorithm
 * @list: Double pointer to the head of the doubly linked list
 */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *current, *temp;
+	listint_t *current, *temp;
 
-        if (list == NULL || *list == NULL || (*list)->next == NULL)
-                return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-        current = (*list)->next;
-        while (current != NULL)
-        {
-                temp = current;
-                while (temp->prev != NULL && temp->n < temp->prev->n)
-                {
-                        temp->prev->next = temp->next;
-                        if (temp->next != NULL)
-                                temp->next->prev = temp->prev;
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
 
-                        temp->next = temp->prev;
-                        temp->prev = temp->prev->prev;
-                        temp->next->prev = temp;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-                        if (temp->prev != NULL)
-                                temp->prev->next = temp;
-                        else
-                                *list = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
 
-                        print_list(*list);
-                }
-                current = current->next;
-        }
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
