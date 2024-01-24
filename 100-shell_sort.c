7c05a362a528aa25e83f11ec609198ad75f819f7
Eltrone <amineelorche@gmail.com>
2024-01-23 18:07:04 -0800

    file added

---
+++ 100-shell_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	for (gap = 1; gap < size; gap = gap * 3 + 1);
+	while (gap > 1)
+	{
+		gap = (gap - 1) / 3;
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
