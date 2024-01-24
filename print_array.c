2fda0a3b13a94fb0728a2f660cc89459a704aef4
Eltrone <amineelorche@gmail.com>
2024-01-23 16:23:04 -0800

    file added

--- print_array.c
+++ print_array.c
@@ -2,22 +2,22 @@
 #include "sort.h"
 
 /**
- * print_array - prints an array of integers
- * @array: The array to be printed
- * @size: The number of elements in the array
- *
- * Description: Prints the elements of an array of integers, followed by a new line.
- * Each element is separated by a comma and a space, with no trailing comma at the end.
- */
+* print_array - prints an array of integers
+* @array: The array to be printed
+* @size: The number of elements in the array
+*
+* Description: Prints elements of array of integers followed by a new line.
+* Each element separated by comma andspace, with no trailing comma at the end.
+*/
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
 }
