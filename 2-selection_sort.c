67d459543adbd1cc84aac8271423e6131162ac46
Eltrone <amineelorche@gmail.com>
2024-01-23 16:43:02 -0800

    file added

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,38 +1,31 @@
 #include "sort.h"
 
 /**
-* insertion_sort_list - Sorts a doubly linked list of integers
-*                       in ascending order using the Insertion sort algorithm
-* @list: Double pointer to the head of the doubly linked list
+* selection_sort - Sorts an array of integers in ascending order
+* using the Selection sort algorithm
+* @array: The array to be sorted
+* @size: Number of elements in @array
 */
-void insertion_sort_list(listint_t **list)
+void selection_sort(int *array, size_t size)
 {
-	listint_t *current, *temp;
+    size_t i, j, min_idx;
+    int temp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
 
-	current = (*list)->next;
-	while (current != NULL)
-	{
-		temp = current;
-		while (temp->prev != NULL && temp->n < temp->prev->n)
-		{
-			temp->prev->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
-			
-			if (temp->prev != NULL)
-				temp->prev->next = temp;
-			else
-				*list = temp;
-
-			print_list(*list);
-		}
-		current = current->next;
-	}
+        if (min_idx != i)
+        {
+            temp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = temp;
+            print_array(array, size);  // Assuming print_array is provided
+        }
+    }
 }
